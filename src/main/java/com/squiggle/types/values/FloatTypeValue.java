4cd9e962a1a29aa0acfabdc5c756cfa265c708c5
NicoMadd <nicomad.17@gmail.com>
2023-07-10 18:35:08 -0300

    # Fix comma-dot separation

--- src/main/java/com/squiggle/types/values/FloatTypeValue.java
+++ src/main/java/com/squiggle/types/values/FloatTypeValue.java
@@ -1,6 +1,8 @@
 package com.squiggle.types.values;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 import com.squiggle.output.Output;
 
@@ -9,6 +11,7 @@ public class FloatTypeValue implements TypeValue {
     Float floatValue;
     static String DEFAULT_FORMAT = "#.#######";
     static String format = DEFAULT_FORMAT;
+    static boolean commaSeparated = false;
     static DecimalFormat decimalFormat = new DecimalFormat(format);
 
     public FloatTypeValue(Float floatValue) {
@@ -17,10 +20,17 @@ public class FloatTypeValue implements TypeValue {
 
     @Override
     public void write(Output out) {
-        // String str = String.format("%f", this.floatValue);
         String str = decimalFormat.format(floatValue);
-        out.print(str);
+        out.print(commaSeparated ? str : str.replaceAll(",", "."));
 
+    }
+
+    public static void setAsCommaSeparated() {
+        commaSeparated = true;
+    }
+
+    public static void setAsDotSeparated() {
+        commaSeparated = false;
     }
 
     public static void setFormat(String string) {
